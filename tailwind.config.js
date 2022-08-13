7c77a24b4bfab56a802bd37ed65062aec10969d1
zulhijaya <zulhijy@gmail.com>
2022-08-13 14:40:05 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['index.html', 'about-us.html'],
+    theme: {
+        extend: {
+            fontFamily: {
+                urbanist: ['Urbanist']
+            },
+        },
+    },
+    plugins: [],    
+}
